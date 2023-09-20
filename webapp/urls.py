6cd44161f908f474527bebf37c9977d8aa7b5160
Nimesh-1 <nimesh10pandit@gmail.com>
2023-09-20 12:18:23 +0545

    login and register

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,28 +5,13 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.home, name=""),
+    path('', views.my_login, name="my-login"),
 
     path('register', views.register, name="register"),
 
-    path('my-login', views.my_login, name="my-login"),
+    path('home', views.home, name="home"),
 
     path('user-logout', views.user_logout, name="user-logout"),
-
-    # CRUD
-
-    path('dashboard', views.dashboard, name="dashboard"),
-
-    path('create-record', views.create_record, name="create-record"),
-
-    path('update-record/<int:pk>', views.update_record, name='update-record'),
-
-    path('record/<int:pk>', views.singular_record, name="record"),
-
-    path('delete-record/<int:pk>', views.delete_record, name="delete-record"),
-
-    
-
 ]
 
 
